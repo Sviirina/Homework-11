3e3d352515f1b9f4af08a579b2858a0fde755713
Sviirina <vozilkina@inbox.ru>
2022-10-31 14:21:06 +0700

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        getYear(1978);
+        System.out.println("Задача 2");
+        chekClientDeviceYear(2015);
+        checkOs(0);
+        System.out.println();
+        System.out.println("Задача 3");
+        calculateDeliveryDays(12);
+    }
+
+    public static void getYear(int year) {
+        if (year % 4 == 0 && year % 100 != 100 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+    public static void chekClientDeviceYear (int clientDeviceYear) {
+        if (clientDeviceYear < 2015) {
+            System.out.print("Установите облегченную версию приложения ");
+        } else {
+            System.out.print("Установите версию приложения для ");
+        }
+    }
+    public static void checkOs (int oS) {
+        if (oS == 0) {
+            System.out.print("iOS по ссылке.");
+        } else {
+            System.out.print(" Android по ссылке");
+        }
+    }
+    public static void calculateDeliveryDays (int deliveryDistance) {
+        int deliveryDays = 1;
+        if (deliveryDistance > 20) {
+            deliveryDays++;
+        }
+        if (deliveryDistance > 60 ) {
+            deliveryDays++;
+        }
+        System.out.println("Потребуется дней: " + deliveryDays);
     }
 }
