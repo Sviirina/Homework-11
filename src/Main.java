8e2e0eaed4a96aa739783a987fb2bf9f9b84366b
Sviirina <vozilkina@inbox.ru>
2022-11-04 14:51:29 +0700

    Исправлено дз

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,11 @@ public class Main {
         chekClientDeviceYear(2015);
         checkOs(0);
         System.out.println();
+        System.out.println("Задача 2 / исправлено");
+        checkApplicationVersion(2010, 1);
+        System.out.println();
         System.out.println("Задача 3");
-        calculateDeliveryDays(12);
+        calculateDeliveryDays(62);
     }
 
     public static void getYear(int year) {
@@ -18,28 +21,47 @@ public class Main {
             System.out.println(year + " год не является високосным");
         }
     }
-    public static void chekClientDeviceYear (int clientDeviceYear) {
+
+    public static void chekClientDeviceYear(int clientDeviceYear) {
         if (clientDeviceYear < 2015) {
             System.out.print("Установите облегченную версию приложения ");
         } else {
             System.out.print("Установите версию приложения для ");
         }
     }
-    public static void checkOs (int oS) {
+
+    public static void checkOs(int oS) {
         if (oS == 0) {
             System.out.print("iOS по ссылке.");
         } else {
             System.out.print(" Android по ссылке");
         }
     }
-    public static void calculateDeliveryDays (int deliveryDistance) {
+
+    public static void checkApplicationVersion (int clientDeviceYear, int oS) {
+        if (clientDeviceYear < 2015) {
+            System.out.print("Установите облегченную версию приложения ");
+        } else {
+            System.out.print("Установите версию приложения для ");
+        }
+        if (oS == 0) {
+            System.out.print("iOS по ссылке.");
+        } else {
+            System.out.print(" Android по ссылке");
+        }
+
+    }
+
+    public static void calculateDeliveryDays(int deliveryDistance) {
         int deliveryDays = 1;
         if (deliveryDistance > 20) {
             deliveryDays++;
         }
-        if (deliveryDistance > 60 ) {
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
             deliveryDays++;
+        } else {
+            System.out.println("Срок доставки рассчитать невозможно");
         }
-        System.out.println("Потребуется дней: " + deliveryDays);
+        System.out.println("Потребуется дней для доставки: " + deliveryDays);
     }
 }
